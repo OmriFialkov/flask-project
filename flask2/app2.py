038311b62f2633df90acdfadec98815b9f09ff53
omri <omrikofialkov@gmail.com>
2024-11-20 11:51:08 +0200

    aa

--- flask2/app2.py
+++ flask2/app2.py
@@ -11,15 +11,16 @@ def home():
 # API route to fetch random quote
 @app.route('/get-quote')
 def get_quote():
-    api_url = "https://api.quotable.io/random"
-    response = requests.get(api_url)
-
-    # Check for errors in the API request
-    if response.status_code == 200:
-        quote = response.json()
-        return jsonify(quote)  # Return the quote as JSON
-    else:
-        return jsonify({"content": "Error fetching quote", "author": "Unknown"}), 500
+    response = requests.get("http://api.forismatic.com/api/1.0/", params={
+        "method": "getQuote",
+        "format": "json",
+        "lang": "en"
+    })
+    quote = response.json()
+    return jsonify({
+        "content": quote["quoteText"],
+        "author": quote["quoteAuthor"] or "Unknown"
+    })
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
